5d3254a0e43936458dd0602656234efcfa696d9e
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-04-08 13:51:01 -0300

    Creación de la carpeta db y el archivo con los datos del usuario y los productos, faltan completar los comentarios

---
+++ db/db.js
@@ -0,0 +1,135 @@
+let datos = {
+    usuario: {
+        email: 'julioperez@gmail.com', 
+        usuario: 'juliope',
+        contrasena: 'elMascrack',
+        fechaDeNacimiento: '2000-05-25',
+        DNI: 40887455,
+        fotoDePerfil: './public/images/foto_de_perfil.jpg'
+    },
+
+    productos: [
+        {
+            producto: 'Lapiz negro',
+            descripcion: 'El Lápiz Negro Noris Staedtler es la elección perfecta para aquellos que buscan calidad y precisión en su escritura o dibujo.',
+            imagen: './public/images/lapiz_negro.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Lapices de colores x12',
+            descripcion: 'Este set de lápices de alta calidad te permitirá dar rienda suelta a tu creatividad y expresarte a través del arte.',
+            imagen: './public/images/lapices_colores.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Marcadores de colores x12',
+            descripcion: 'Estos marcadores de doble punta, una biselada y otra redonda, son ideales para una amplia gama de aplicaciones artísticas como manga, retratos, bocetos, dibujo y diseño.',
+            imagen: './public/images/marcadores_colores.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Marcador indeleble negro',
+            descripcion: 'Este marcador permanente es de alta calidad y durabilidad.',
+            imagen: './public/images/marcador_indeleble.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Tijera',
+            descripcion: 'Esta tijera es un producto de alta calidad, ideal para uso en oficinas, comercios o el hogar.',
+            imagen: './public/images/tijera.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Voligoma',
+            descripcion: 'Adhesivo sintético voligoma transparente x30 ml.',
+            imagen: './public/images/voligoma.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Goma de borrar',
+            descripcion: 'Este producto de alta calidad, de color blanco y forma rectangular, viene en un pack de dos unidades, lo que garantiza que siempre tendrás una a mano.',
+            imagen: './public/images/goma_de_borrar.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Hojas rayadas A4',
+            descripcion: 'Este repuesto escolar es un producto de alta calidad, diseñado para satisfacer las necesidades tanto de estudiantes como de profesionales.',
+            imagen: './public/images/hojas_rayadas.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Hojas cuadriculadas A4',
+            descripcion: 'Este repuesto escolar es un producto de alta calidad, diseñado para satisfacer las necesidades tanto de estudiantes como de profesionales, ideales para calculos matematicos y/o graficar.',
+            imagen: './public/images/hojas_cuadriculadas.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        },
+        {
+            producto: 'Resaltador amarillo',
+            descripcion: 'Su diseño ergonómico permite un uso cómodo y prolongado. La tinta de alta calidad asegura una aplicación suave y uniforme, sin manchas ni desvanecimientos.',
+            imagen: './public/images/resaltador_amarillo.jpg',
+            comentarios: [
+                {
+                    nombreUsuario: 1,
+                    texto: 1,
+                    imagenPerfil: 1
+                }
+            ]
+        }
+    ]
+};
+
+module.exports = datos;
