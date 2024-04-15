c27fa5b1aaf15449f61c5e3786d21593c74925cd
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-04-15 12:31:09 -0300

    search results

--- routes/product.js
+++ routes/product.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.home);
+router.get('/searchResults', productController.searchResults);
 
 module.exports = router;
