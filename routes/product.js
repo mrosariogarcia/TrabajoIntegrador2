abe82d845486ea76ed72b7b2f5a0a1d8ae03db86
mrosariogarcia <garciamr@udesa.edu.ar>
2024-04-16 11:44:17 -0300

    Partials y arreglos

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.todosProducto);
-router.get('/:id', productController.detalle);
+//router.get('/:id', productController.detalle);
 router.get('/searchResults', productController.searchResults);
 
 module.exports = router;
