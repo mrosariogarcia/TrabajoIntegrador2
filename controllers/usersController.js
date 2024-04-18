977e67320f201288686fb54aa938d3dd003804b4
juanagrether <jgrether@udesa.edu.ar>
2024-04-18 08:52:42 -0300

    modificacion de sql y profile

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-/* Normalmente, acá "importaríamos la base de datos" */
+/* importaríamos la base de datos */
 
 let db = require("../db/db"); //importando la lista, para mandarla a renderizar en mi objeto literal, para mostrar productos 
 
