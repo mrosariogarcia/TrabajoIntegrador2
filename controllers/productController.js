64a55e0b1208dbfb9376fe94943ec18dc3e9e784
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-04-17 22:24:02 -0300

    seguimos con search results

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,55 +2,40 @@ let db = require("../db/db"); //importando la lista, para mandarla a renderizar 
 
 let productController = {
     producto: function(req, res) {
-        return res.render('product', 
-        {info:db})
+        return res.render('product', {
+            info:db
+        })
     },
     
-    searchResults:function(req,res,next){
-        let searchResults = req.params.searchResults;
-        
-        let resultado = [];
+    searchResults:function(req,res){
+        res.render('search-results', {
+            info:db.productos
+        })
+    },
 
-        for (let i = 0; i < db.productos.length; i++) {
-            let productoABuscar = db.productos[i].producto;
-            
-            console.log(productoABuscar == searchResults);
+    detalle:function(req,res){
+        let idEnviado = req.params.id;
+        let productoEnviado = req.params.producto;
+        let detalleProducto = [];
 
-            if (searchResults == productoABuscar){
-                resultado.push(db.productos[i])
-            }
-        }
-        console.log(resultado);
-
-        if (resultado.length >= 1){
-            return res.render("search-results", {
-                mensaje: `Éstos son los resultados que encontramos para ${searchResults}`,
-                resultados: resultado
+        for (let i = 0; i < db.products.length; i++) {
+            let idProducto = db.productos[i].id;
+            if (idEnviado == idProducto){
+                detalleProducto.push(db.productos[i])
+            }      
+        };
+        if (detalleProducto.length >= 1){
+            return res.render('product', {
+                mensaje: `Éste es el detalle del producto ${productoEnviado}.`,
+                info:detalleProducto
             })
         }
         else {
-            return res.render("search-results", {
-                mensaje: `No hay productos para para ${searchResults}`,
-                resultados: resultado
+            return res.render('product', {
+                mensaje: `No encontramos detalle del producto ${productoEnviado}.`,
+                info:detalleProducto
             })
-        } 
-    },
-    detalle:function (req,res) {
-        let idEnviado = req.params.id
-        let detalleProducto = []
-        for (let i = 0; i < db.productos.length; i++) {
-            if (idEnviado == db.productos[i].id) {
-                detalleBanda.push(db.productos[i])
-                return res.render('product',{
-                    info:detalleProducto
-                })
-            }
         }
-        return res.render('idNovalido',{
-            mensaje:'No existe el id:'+ idEnviado +', intente nuevamente con otro.'
-        })
-        
-        
     }
 
     // <% for(let i=0; i < info.productos.length; i++ ) { %>
