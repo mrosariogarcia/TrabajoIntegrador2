01e2388b280cf17c7eca8c9dee275078074935e7
juanagrether <jgrether@udesa.edu.ar>
2024-06-20 00:45:58 -0300

    arreglos de logueado con search y detalle

--- controllers/productController.js
+++ controllers/productController.js
@@ -39,27 +39,28 @@ let productController = {
             return res.render('search-results', { productos: [], buscado: busqueda });
         });
 },
-    detalle: function (req, res) {
-        let id = req.params.id;
+detalle: function (req, res) {
+    let id = req.params.id;
 
-        db.Product.findByPk(id, {
-            // agregamos includes de generos y actores
-            include: [
-                { association: 'usuario' },
-                { association: 'comentarios', include:[ { association: 'user' }] },
-            ]
+    db.Product.findByPk(id, {
+        // agregamos includes de generos y actores
+        include: [
+            { association: 'usuario' },
+            { association: 'comentarios', include:[ { association: 'user' }] },
+        ]
+    })
+        .then(data => {
+            let si = false;
+            if (req.session.user != undefined && req.session.user.id == data.usuario.id) {
+                si = true;
+            }
+            console.log("data: ", JSON.stringify(data, null, 4));
+            console.log("coments: ", JSON.stringify(data.comentarios, null, 4));
+            return res.render('product', { resultado: data, comentarios: data.comentarios, si: si });
         })
-            .then(data => {
-                if (req.session.user != undefined && req.session.user.id == results.usuario.id) {
-                    si = true;
-                }
-                else{ si=false}
-            console.log("coments: ", JSON.stringify(data.comentarios,null, 4))
-                return res.render('product', { resultado: data, comentarios: data.comentarios,si:si});
-            })
-            .catch(error => {
-                console.log(error);
-            })
+        .catch(error => {
+            console.log(error);
+        })
     },
 
 
