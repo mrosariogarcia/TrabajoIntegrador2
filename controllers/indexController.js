b435f0415e0bf3f9828d4bc6a650768db0a6872d
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-06-22 18:17:54 -0300

    orden decendiente de los productos

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,33 @@
 let db = require("../database/models")
 
 let indexController= {
-
-    home: function(req, res) {
-        db.Product.findAll()
+    home: function(req, res, next){
+        let filtro = {
+            order: [['createdAt', 'DESC']],
+            include: [
+                { association: "comentarios" },
+                { association: "usuario" }
+            ]
+        }
+        db.Product.findAll(filtro)
             .then(function(data){
-                res.render('index',{productos:data})
+                return res.render('index',{productos:data})
             })    
             .catch(function (error) {
                 console.log(error);
               }); 
-    },
+
+    }
+
+    // home: function(req, res) {
+    //     db.Product.findAll()
+    //         .then(function(data){
+    //             res.render('index',{productos:data})
+    //         })    
+    //         .catch(function (error) {
+    //             console.log(error);
+    //           }); 
+    // },
 }
 
 module.exports = indexController
